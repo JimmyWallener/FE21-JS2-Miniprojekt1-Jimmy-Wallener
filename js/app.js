8fe8290d0a147a6b23a5e122aa7e87a4e3d9ad28
Jimmy Wallener <38855083+JimmyWallener@users.noreply.github.com>
2022-02-06 12:20:49 +0100

    Add new gamefeature

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
-const body = document.querySelector("body");
-const rpsImage = document.querySelector(".rps-img");
+(function () {
+  const gameRedirect = () => {
+    let player = localStorage.name;
+    if (typeof player === 'string') {
+      window.open('game.html', '_self');
+    }
+  };
+  const playerName = () => {
+    let name = prompt('Please enter your name:');
 
-function gameRedirect() {
-  let player = localStorage.name;
-  if (typeof player === "string") {
-    window.open("game.html", "_self");
-  }
-}
-function playerName() {
-  let name = prompt("Please enter your name:");
+    if (name == null || name == '') {
+      const randomizedNumber = Math.round(Math.random() * 1000);
+      localStorage.setItem('name', `Player_${randomizedNumber}`);
+    } else {
+      localStorage.setItem('name', name);
+    }
+    setTimeout(gameRedirect, 2000);
+  };
 
-  if (name == null || name == "") {
-    localStorage.setItem("name", "Player One");
-  } else {
-    localStorage.setItem("name", name);
-  }
-  setTimeout(gameRedirect, 2000);
-}
-
-rpsImage.addEventListener("click", (e) => {
-  e.preventDefault();
-  playerName();
-});
+  document.querySelector('.rps-img').addEventListener('click', (e) => {
+    e.preventDefault();
+    playerName();
+  });
+})();
--- js/game.js
+++ js/game.js
@@ -1,119 +1,168 @@
-const playRound = document.querySelector(".round");
-const playerCards = document.querySelectorAll(".player");
-const playerOneScore = document.querySelector(".player-one");
-const computerScore = document.querySelector(".computer");
-const computerChoice = document.querySelector(".computer-choice");
-const versus = document.querySelector(".versus");
+(function () {
+  const playerCards = document.querySelectorAll('.player');
+  const playerScore = document.querySelector('.player-one');
+  const cpuChoice = document.querySelector('.computer-choice');
+  const versus = document.querySelector('.versus');
 
-const h1 = document.createElement("h1");
-playRound.appendChild(h1);
+  const h2 = document.createElement('h1');
+  playerScore.appendChild(h2);
 
-const h2 = document.createElement("h1");
-playerOneScore.appendChild(h2);
+  const h4 = document.createElement('h1');
+  versus.appendChild(h4);
+  h4.setAttribute('class', 'result-text');
 
-const h3 = document.createElement("h1");
-computerScore.appendChild(h3);
-
-const h4 = document.createElement("h1");
-versus.appendChild(h4);
-h4.setAttribute("class", "result-text");
-
-const game = {
-  playerName: localStorage.name,
-  player: 0,
-  cpu: 0,
-  round: 0,
-  playCards: ["rock", "paper", "scissors"],
-  winner: function () {
-    if (this.player === 3 || this.cpu === 3) {
-      const win = this.player === 3 ? "You win the game" : "CPU wins the game";
-      alert(win);
-      const vs = document.querySelector(".result-text");
-      vs.innerText = "";
-      return true;
+  // Instance class for creating database urls depending on requested data
+  class Database {
+    constructor(prefix, endpoint) {
+      this.url =
+        'https://exercise-js2-default-rtdb.europe-west1.firebasedatabase.app/';
+      this.prefix = prefix;
+      this.endpoint = endpoint;
     }
-    return false;
-  },
-  scoreBoard: function () {
-    h2.innerText = `${this.playerName}:  ${this.player}`;
-    h1.innerText = `Round ${this.round}`;
-    h3.innerText = `Computer: ${this.cpu}`;
-  },
-  resetGame: function () {
-    const vs = document.querySelector(".result-text");
-    this.round = 0;
-    this.player = 0;
-    this.cpu = 0;
-    vs.innerText = "";
-    this.scoreBoard();
-  },
-  randomNumber: function () {
-    return Math.floor(Math.random() * this.playCards.length);
-  },
-};
-/*
+    getUri() {
+      return `${this.url}/${this.prefix}/${this.endpoint}.json`;
+    }
+  }
+
+  const game = {
+    _name: localStorage.name,
+    _playerScore: 0,
+    cards: ['rock', 'paper', 'scissors'],
+    // If player wins, add to score, if tie do nothing. If CPU wins, check highscore and update if needed.
+    winner: function (player, computer) {
+      if (gameLogic(player, computer)) {
+        this._playerScore++;
+        this.scoreBoard();
+      } else if (gameLogic(player, computer) === null) {
+        this.scoreBoard();
+      } else {
+        compareHighscore(this._playerScore, this._name);
+        this._playerScore = 0;
+        this.scoreBoard();
+      }
+    },
+    scoreBoard: function () {
+      h2.innerText = `${this._name}:  ${this._playerScore}`;
+    },
+    randomNumber: function () {
+      return Math.floor(Math.random() * this.cards.length);
+    },
+  };
+
+  const clickCard = (card, index) => {
+    card.addEventListener('click', () => {
+      game.winner(index, computerChoice());
+    });
+  };
+
+  /*
  Lets append <img> to each <div>, with clicklisteners on each element
  for picking up users choice, getting index for reference to game logic.
 */
-
-playerCards.forEach((card, index) => {
-  const img = document.createElement("img");
-  card.appendChild(img);
-  img.setAttribute("src", `./img/${game.playCards[index]}.png`);
-  img.setAttribute("alt", `${game.playCards[index]}`);
-  img.classList.add("card");
-  game.scoreBoard();
-  clickCard(card, index);
-});
-
-function clickCard(card, index) {
-  card.addEventListener("click", () => {
+  playerCards.forEach((card, index) => {
+    const img = document.createElement('img');
+    card.appendChild(img);
+    img.setAttribute('src', `./img/${game.cards[index]}.png`);
+    img.setAttribute('alt', `${game.cards[index]}`);
+    img.classList.add('card');
     game.scoreBoard();
-    if (!game.winner()) {
-      game.round++;
-      gameLogic(index, computerLogic());
-    }
+    clickCard(card, index);
   });
-}
 
-// Create random number and use that to create <img> based on cpu choice
-function computerLogic() {
-  let randomNumber = game.randomNumber();
-  let img = document.createElement("img");
+  // Create random number and use that to create <img> based on cpu choice
+  const computerChoice = () => {
+    let randomNumber = game.randomNumber();
+    let img = document.createElement('img');
 
-  while (computerChoice.firstChild) {
-    computerChoice.removeChild(computerChoice.lastChild);
-  }
-  computerChoice.appendChild(img);
-  img.setAttribute("src", `./img/${game.playCards[randomNumber]}.png`);
-  img.setAttribute("alt", `${game.playCards[randomNumber]}`);
-  img.setAttribute("class", "card cpu-card");
-  return randomNumber;
-}
+    while (cpuChoice.firstChild) {
+      cpuChoice.removeChild(cpuChoice.lastChild);
+    }
+    cpuChoice.appendChild(img);
+    img.setAttribute('src', `./img/${game.cards[randomNumber]}.png`);
+    img.setAttribute('alt', `${game.cards[randomNumber]}`);
+    img.setAttribute('class', 'card cpu-card');
+    return randomNumber;
+  };
 
-// Game logic for checking win/loss/tie
+  // Game logic for checking win/loss/tie
+  const gameLogic = (player, computer) => {
+    let winner = '';
+    if (player === computer) {
+      h4.innerText = 'Tie';
+      winner = null;
+    } else if (player === 0 && computer === 2) {
+      h4.innerText = 'You Win';
+      winner = true;
+    } else if (player === 1 && computer === 0) {
+      h4.innerText = 'You Win';
+      winner = true;
+    } else if (player === 2 && computer === 1) {
+      h4.innerText = 'You Win';
+      winner = true;
+    } else {
+      h4.innerText = 'You loose';
+      winner = false;
+    }
+    return winner;
+  };
 
-function gameLogic(player, computer) {
-  if (player === computer) {
-    h4.innerText = "Tie";
-  } else if (player === 0 && computer === 2) {
-    game.player++;
-    h4.innerText = "You Win";
-  } else if (player === 1 && computer === 0) {
-    game.player++;
-    h4.innerText = "You Win";
-  } else if (player === 2 && computer === 1) {
-    game.player++;
-    h4.innerText = "You Win";
-  } else {
-    game.cpu++;
-    h4.innerText = "You loose";
-  }
-}
+  // Function gets name & score, compare with scores in database. If score is higher, update database and break loop, if equal, break loop
+  const compareHighscore = async (playerScore, playerName) => {
+    const connect = new Database('highscore', '');
 
-// Reset Button
-const resetBtn = document.querySelector("#reset-btn");
+    await fetch(connect.getUri())
+      .then((res) => res.json())
+      .then((results) => {
+        for (const result in results) {
+          const { score } = results[result];
+          if (playerScore > score) {
+            updateHighscore(playerScore, playerName, result);
+            break;
+          } else if (playerScore === score) {
+            break;
+          }
+        }
+      });
+  };
 
-resetBtn.addEventListener("click", () => {
-  game.resetGame();
-});
+  // Function sends PATCH to database with new score and name, updates scoreboard afterwards.
+  const updateHighscore = async (playerScore, playerName, index) => {
+    const connect = new Database('highscore', index);
+
+    await fetch(connect.getUri(), {
+      method: 'PATCH',
+      body: JSON.stringify({
+        name: playerName,
+        score: playerScore,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        getHighscoreBoard();
+      });
+  };
+
+  // Removes any existing children from DOM-Element, then populates it with new data-lists
+  const getHighscoreBoard = async () => {
+    const connect = new Database('highscore', '');
+    const ol = document.querySelector('.scores');
+    while (ol.firstChild) {
+      ol.removeChild(ol.lastChild);
+    }
+
+    await fetch(connect.getUri())
+      .then((res) => res.json())
+      .then((users) => {
+        for (const user in users) {
+          const li = document.createElement('li');
+          const { name, score } = users[user];
+          li.innerText = `${name} with ${score} rounds`;
+          ol.appendChild(li);
+        }
+      });
+  };
+  getHighscoreBoard();
+})();
